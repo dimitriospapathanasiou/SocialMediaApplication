2310ea03e778a761816a72dc632e628fd669ce92
dimitriospapathanasiou <116485010+dimitriospapathanasiou@users.noreply.github.com>
2023-01-15 23:28:16 +0200

    Update SearchUsers

--- app/src/main/java/com/aueb/socialmediaapplication/Activities/SearchUsers.java
+++ app/src/main/java/com/aueb/socialmediaapplication/Activities/SearchUsers.java
@@ -1,5 +1,7 @@
 package com.aueb.socialmediaapplication.Activities;
 
+import android.util.Log;
+
 import com.aueb.socialmediaapplication.Database.UserDatabase;
 import com.aueb.socialmediaapplication.Entities.Message;
 import com.aueb.socialmediaapplication.Entities.User;
@@ -10,37 +12,59 @@ public class SearchUsers {
     ContactInterface view;
     UserDatabase userDB;
 
+    //Initialisation of variables
     public SearchUsers(ContactInterface view, UserDatabase usdb) {
         this.view = view;
         this.userDB = usdb;
     }
 
+    //Processes user list for ContactList catalogue
+    //Adds a user depending on Receiver-Sender status
+    //of a message
     public void loadUsers(User user) {
         ArrayList<User> userList= new ArrayList<User>();
         ArrayList<User> allUsers = userDB.getAllUsers();
-        ArrayList<Message> messagesReceived = user.getMessagesReceived();
-
+        Log.d("allusers", String.valueOf(allUsers));
+        ArrayList<Message> messagesReceived = user.getMessagesReceived(user.getUserName());
+        ArrayList<Message> messagesSent = user.getMessagesSent(user.getUserName());
+        Log.d("userId2",String.valueOf(user.getUserId()));
+        Log.d("messagesReceived", String.valueOf(messagesReceived));
         for(User u: allUsers) {
             for (Message m : messagesReceived) {
                 if(!isUserInList(u.getUserName(),userList)) {
-                    if (user.getUserName().equals(m.getSender()))
-                        userList.add(u);
-                    if (user.getUserName().equals(m.getReceiver()))
+                    Log.d("avocado4", String.valueOf(user.getUserName()));
+                    Log.d("avocado5", String.valueOf(m.getSender()));
+                    if (u.getUserName().equals(m.getSender()))
                         userList.add(u);
                 }
             }
         }
-
+        for(User u: allUsers) {
+            for (Message m : messagesSent) {
+                if(!isUserInList(u.getUserName(),userList)) {
+                    Log.d("avocado4", String.valueOf(user.getUserName()));
+                    Log.d("avocado5", String.valueOf(m.getReceiver()));
+                    if (u.getUserName().equals(m.getReceiver()))
+                        userList.add(u);
+                }
+            }
+        }
         userList.remove(user);
+        Log.d("avocado3", String.valueOf(userList));
+        for(User u: userList){
+            Log.d("avocado", u.getUserName());
+            Log.d("avocado2", String.valueOf(u.getUserId()));
+        }
 
         if(!userList.isEmpty())
             view.showUsers(userList);
     }
 
-    public static boolean isUserInList(String name, ArrayList<User> users) {
+    //Checks whether the given user exists on ArrayList
+    public static boolean isUserInList(String username, ArrayList<User> users) {
 
         for (User u : users) {
-            if (u.getUserName().equals(name)) {
+            if (u.getUserName().equals(username)) {
                 return true;
             }
         }
